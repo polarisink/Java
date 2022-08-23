ce1f8a9eb2150dffcdf73b89deafe60355bda98b
aries <polarisink@163.com>
2022-08-23 17:45:43 +0800

    :alembic: 虚假唤醒

---
+++ juc/src/main/java/github/polarisink/thread/ThreadActiveCount.java
@@ -0,0 +1,22 @@
+package github.polarisink.thread;
+
+/**
+ * @author hzsk
+ */
+public class ThreadActiveCount {
+    public static void main(String[] args) {
+        //打印当前线程组的线程
+        Thread.currentThread().getThreadGroup().list();
+        System.out.println("=========");
+        //idea用的是反射,还有一个monitor监控线程。
+        System.out.println(Thread.activeCount());
+        /*
+        输出结果:
+        java.lang.ThreadGroup[name=main,maxpri=10]
+            Thread[main,5,main]
+            Thread[Monitor Ctrl-Break,5,main]
+        =========
+        2
+        * */
+    }
+}
