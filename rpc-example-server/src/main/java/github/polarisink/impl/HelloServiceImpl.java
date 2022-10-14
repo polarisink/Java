923f5c1a4dd5cba725c25f1225887c2b96fd0660
aries <polarisink@163.com>
2022-10-14 14:14:50 +0800

    <type>(<rpc-example-server>): <testMsg>
    <msgBody>
    
    <msgfooter>

--- rpc-example-server/src/main/java/github/polarisink/impl/HelloServiceImpl.java
+++ rpc-example-server/src/main/java/github/polarisink/impl/HelloServiceImpl.java
@@ -17,6 +17,7 @@ public class HelloServiceImpl implements HelloService {
         System.out.println("test flow");
         System.out.println("test flow2");
         System.out.println("test flow3");
+        System.out.println("test flow4");
     }
 
     @Override
